e561f7db1ac97d919ff1195013c8e4bc2264b57b
memo <memo33@users.noreply.github.com>
2024-02-17 11:16:01 +0100

    bump version to 1.0.3

--- src/version.h
+++ src/version.h
@@ -20,6 +20,6 @@
 
 #pragma once
 
-#define PLUGIN_VERSION_STR      "1.0.2"
-#define RESOURCE_VERSION         1,0,2,0
-#define RESOURCE_VERSION_STR    "1.0.2.0"
+#define PLUGIN_VERSION_STR      "1.0.3"
+#define RESOURCE_VERSION         1,0,3,0
+#define RESOURCE_VERSION_STR    "1.0.3.0"
